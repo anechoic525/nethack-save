8c6c1b03762a3fe76e39a6af9f86c7d9d5c86a19
anechoic525 <joe.reyenga@gmail.com>
2020-03-14 04:46:21 +0000

    updated download script

--- nethack-git.sh
+++ nethack-git.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+apt-get install nethack
+cd /var/games/nethack/save
 git config --global user.name "anechoic525"
 git config --global user.email "joe.reyenga@gmail.com"
 git clone https://github.com/anechoic525/nethack-save
